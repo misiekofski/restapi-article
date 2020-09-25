453860c345ccc3dc4f94d4f68d29df7f9cee2bc2
Michal Dobrzycki <dobrzycki@gmail.com>
2020-09-25 21:35:04 +0200

    Added some tests

--- restapi/tests.py
+++ restapi/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+from .models import Profile
+
+
+@pytest.fixture
+def api_client():
+    from rest_framework.test import APIClient
+    return APIClient()
+
+
+@pytest.mark.django_db
+def test_user_can_update_his_profile(api_client):
+    user = User.objects.create_user('michal', 'test@scvconsultants.com', 'michalpassword')
+    url = reverse('profile-detail', args=[user.id])
+    response = api_client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_user_create_creates_profile():
+    User.objects.create_user('michal', 'test@scvconsultants.com', 'michalpassword')
+    assert Profile.objects.count() == 1
+    assert User.objects.count() == 1
